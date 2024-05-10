740a69875a0421d1c78e44ed0054473ce942dcd8
Carol-Yin000 <carolyin.000@gmail.com>
2024-05-10 14:08:20 -0400

    add Package controller and Class HomeController

---
+++ springmono/src/main/java/net/nvsoftware/springmono/controller/HomeController.java
@@ -0,0 +1,17 @@
+package net.nvsoftware.springmono.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "nvsoftware Home";
+    }
+
+}
+
